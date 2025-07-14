b38119a638a79875944c0e7ae0f25f294a954aae
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2025-07-14 15:52:28 -0300

    create qrcode

--- src/prompts/prompt-qrcode.js
+++ src/prompts/prompt-qrcode.js
@@ -1,5 +1,17 @@
 import chalk from "chalk";
 
-const promptQRCode = []
+const promptQRCode = [
+    {
+        name : "link",
+        description : chalk.yellow("Digite o link para gerar o QRCode"),
+    },
+    {
+        name : "type",
+        description : chalk.yellow.bold("Escolha entre o tipo (1- Normal ou (2- Terminal"),
+        pattern : /^[1-2]+$/,
+        message : chalk.red.italic("Escolha apenas entre 1 e 2"),
+        required : true
+    }
+];
 
 export default promptQRCode;
--- src/services/qr-code/create.js
+++ src/services/qr-code/create.js
@@ -1,8 +1,12 @@
 import prompt from "prompt";
 import promptQRCode from "../../prompts/prompt-qrcode.js";
+import handle from "./handle.js";
+
 
 async function createQrcode() {
-    
+    prompt.get(promptQRCode, handle);
+
+    prompt.start();
 }
 
 export default createQrcode;
