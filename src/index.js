928d3be8ae6843aa3d1b7fa2d51b273801bb7825
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2025-07-14 15:39:21 -0300

    crreate services

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import prompt from "prompt";
 import mainPrompt from "./prompts/prompt-main.js";
+import createQrcode from "./services/qr-code/create.js";
 
 async function main() {
     prompt.get(mainPrompt, async (err, chose) => {
-        if(chose.select == 1) console.log("Escolheu QRCODE");
+        if(chose.select == 1) await createQrcode();
         if(chose.select == 2) console.log("Escolheu PASSWORD");
     })
 }
---
+++ src/services/qr-code/create.js
@@ -0,0 +1,8 @@
+import prompt from "prompt";
+import promptQRCode from "../../prompts/prompt-qrcode.js";
+
+async function createQrcode() {
+    
+}
+
+export default createQrcode;
