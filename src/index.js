d5987c793daba80ec42790fd827d3873f874b194
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2025-07-14 15:25:23 -0300

    test main

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import prompt from "prompt";
-
+import mainPrompt from "./prompts/prompt-main.js";
 
 async function main() {
-    
+    prompt.get(mainPrompt, async (err, chose) => {
+        if(chose.select == 1) console.log("Escolheu QRCODE");
+        if(chose.select == 2) console.log("Escolheu PASSWORD");
+    })
 }
 
 main();
