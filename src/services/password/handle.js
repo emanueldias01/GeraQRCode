95ee2d2c852cb3878c7b5d16762e141482b2d454
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2025-07-14 16:12:36 -0300

    generate password done

--- src/services/password/handle.js
+++ src/services/password/handle.js
@@ -1,2 +1,41 @@
 
 
+async function permittedChars() {
+    let chars = [];
+
+    if(process.env.UPPERCASE_LETTERS){
+        chars.push(..."ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    }
+
+    if(process.env.LOWERCASE_LETTERS){
+        chars.push(..."abcdefghijklmnopqrstuvwxyz");
+    }
+
+    if(process.env.NUMBERS){
+        chars.push(..."0123456789");
+    }
+
+    if(process.env.SPECIAL_CHARS){
+        chars.push(..."!@#$%^&*()-_");
+    }
+
+    return chars;
+}
+
+async function handle() {
+    const passwordLength = process.env.PASSWORD_LENGTH;
+
+    const chars = await permittedChars();
+
+    console.log(chars);
+    let password = "";
+
+    for(let i = 0; i < passwordLength; i++){
+        const index = Math.floor(Math.random() * chars.length);
+        password += chars[index];
+    }
+
+    return password;
+}
+
+export default handle;
