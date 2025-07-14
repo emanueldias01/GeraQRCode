ccff94aee98def08665fb07abf9634ddf55870b3
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2025-07-14 15:55:58 -0300

    add function createPassword

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import prompt from "prompt";
 import mainPrompt from "./prompts/prompt-main.js";
 import createQrcode from "./services/qr-code/create.js";
+import createPassword from "./services/password/create.js";
 
 async function main() {
     prompt.get(mainPrompt, async (err, chose) => {
         if(chose.select == 1) await createQrcode();
-        if(chose.select == 2) console.log("Escolheu PASSWORD");
+        if(chose.select == 2) await createPassword();
     })
 }
 
